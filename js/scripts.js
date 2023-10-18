bcfac77be1c430e5b76cc38e0a8930ba3e084a88
Hunter Empey <LizardMak@gmail.com>
2023-10-17 18:01:09 -0600

    darkmode

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 //User interface logic
 window.onload = function() {
- 
+ let lightMode = document.querySelector("#light");
+ let darkMode = document.querySelector("#dark");
+ let body = document.body;
+ let head = document.head;
+ darkMode.onclick = function() {
+  body.style.color = "white";
+  body.style.backgroundColor = "black";
+ };
 };
